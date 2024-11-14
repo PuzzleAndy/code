50d4ce438447e36a0245860afd2120a2e90cc979
PuzzleAndy <michaelstampone@gmail.com>
2024-11-14 00:27:15 -0600

    -

---
+++ general/general_floor_ceil/a.py
@@ -0,0 +1,112 @@
+import math
+from matplotlib import pyplot as plt
+import numpy as np
+
+def pow2(n):
+	return 2 ** n
+
+def odd(n):
+	return 2 * n + 1
+
+def is_even(n):
+	return n % 2 == 0
+
+# f is increasing
+# the domain of f is N
+# the image of f is [f(0), inf)
+def inc_nat_floor(f, x):
+	prev = f(0)
+	if x < prev:
+		return None
+	if x == prev:
+		return prev
+	i = 1
+	while True:
+		y = f(i)
+		if x <= y:
+			return prev
+		prev = y
+		i += 1
+
+# f is increasing
+# the domain of f is N
+# the image of f is [f(0), inf)
+def inc_nat_ceil(f, x):
+	prev = f(0)
+	if x <= prev:
+		return prev
+	i = 1
+	while True:
+		y = f(i)
+		if x <= y:
+			return y
+		prev = y
+		i += 1
+
+# f is increasing
+# the domain of f is Z
+# the image of f is (-inf, inf)
+def inc_int_floor(f, x):
+	prev = f(0)
+	if x == prev:
+		return prev
+	if x > prev:
+		i = 1
+		while True:
+			y = f(i)
+			if x <= y:
+				return prev
+			prev = y
+			i += 1
+	else:
+		i = -1
+		while True:
+			y = f(i)
+			if x >= y:
+				return y
+			prev = y
+			i -= 1
+
+# f is increasing
+# the domain of f is Z
+# the image of f is (-inf, inf)
+def inc_int_ceil(f, x):
+	prev = f(0)
+	if x == prev:
+		return prev
+	if x > prev:
+		i = 1
+		while True:
+			y = f(i)
+			if x <= y:
+				return y
+			prev = y
+			i += 1
+	else:
+		i = -1
+		while True:
+			y = f(i)
+			if x >= y:
+				return prev
+			prev = y
+			i -= 1
+
+def pow2_ceil(x):
+	return inc_nat_ceil(pow2, x)
+
+def odd_floor(x):
+	return inc_int_floor(odd, x)
+
+x = np.linspace(0, 16, 100)
+
+y = np.vectorize(pow2_ceil, otypes=[float])(x)
+plt.clf()
+plt.plot(x, y, color='red')
+plt.savefig('pow2_ceil.png')
+
+x = np.linspace(-5, 5, 100)
+
+y = np.vectorize(odd_floor, otypes=[float])(x)
+plt.clf()
+plt.plot(x, y, color='red')
+plt.savefig('odd_floor.png')
--- graphics/white_noise/a.py
+++ graphics/white_noise/a.py
@@ -1,34 +1,51 @@
-# License: CC0
-# https://puzzleandy.com
-
-# pip install numpy
-# pip install pillow
-
 import numpy as np
-from PIL import Image
+import math
+import cv2
+from enum import Enum
 
-def white_noise_mat(w, h):
-	return np.random.random_sample((h, w))
+class HorPos:
+	LEFT = 0
+	MIDDLE = 1
+	RIGHT = 2
 
-def white_noise_im(w, h):
-	mat = white_noise_mat(w, h)
-	return Image.fromarray(np.uint8(mat * 255))
+class VertPos:
+	BOTTOM = 0
+	MIDDLE = 1
+	TOP = 2
 
-def rgb_noise_mat(w, h):
-	return np.random.random_sample((h, w, 3))
+hor_pos = HorPos.LEFT
+vert_pos = VertPos.BOTTOM
 
-def rgb_noise_im(w, h):
-	mat = rgb_noise_mat(w, h)
-	return Image.fromarray(np.uint8(mat * 255))
+def noise(w, h, s, num_components, hor_pos=HorPos.MIDDLE, vert_pos=VertPos.MIDDLE):
+	img = np.random.random_sample((math.ceil(h / s), math.ceil(w / s), num_components))
+	img = np.uint8(img * 255)
+	img = cv2.resize(img, (img.shape[1] * s, img.shape[0] * s), interpolation=cv2.INTER_NEAREST)
+	if hor_pos == HorPos.LEFT:
+		x = 0
+	elif hor_pos == HorPos.RIGHT:
+		x = s - w % s
+	else:
+		x = math.floor((s - w % s) / 2)
+	if vert_pos == VertPos.BOTTOM:
+		y = 0
+	elif vert_pos == VertPos.TOP:
+		y = s - h % s
+	else:
+		y = math.floor((s - h % s) / 2)
+	img = img[y:y + h, x:x + w]
+	return img
 
-def rgba_noise_mat(w, h):
-	return np.random.random_sample((h, w, 4))
+def grayscale_noise(w, h, s):
+	return noise(w, h, s, 1)
 
-def rgba_noise_im(w, h):
-	mat = rgba_noise_mat(w, h)
-	return Image.fromarray(np.uint8(mat * 255))
+def rgb_noise(w, h, s):
+	return noise(w, h, s, 3)
 
-w = 500
-h = 500
-im = rgb_noise_im(w, h)
-im.save('out.png')
+def rgba_noise(w, h, s):
+	return noise(w, h, s, 4)
+
+w = 2500
+h = 1642
+s = 10
+img = rgb_noise(w, h, s)
+cv2.imwrite('out.png', img)
